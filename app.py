1b50b2f22fbdb6bb7a8c4a2034f9abefff76f81f
christophermorel <144291124+christophermorel@users.noreply.github.com>
2023-12-06 02:32:11 -0500

    ee

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
-import random
 from flask import Flask, render_template, request, jsonify
-from wordle_wordlist import get_word_list
-from cpu import cpu_wordle
-
+import random
 
 app = Flask(__name__)
 
-# Home route - Renders the title screen or handles form submission
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        player_name = request.form.get('player_name', 'Guest')
-        return render_template('wordle.html', player_name=player_name)
-    return render_template('index.html')
+# Sample word list
+word_list = ["APPLE", "LEMON", "TIGER", "WATER", "SNAKE"]
 
+def get_random_word():
+    return random.choice(word_list)
 
-# Wordle route - Renders the play screen
-@app.route('/wordle', methods=['POST'])
-def wordle():
-    player_name = request.form.get('player_name', 'Guest')
-    player_guess = request.form.get('guess', '')
-    return render_template('wordle.html', player_name=player_name)
+@app.route('/')
+def index():
+    return render_template('wordle.html')
 
-# Route for getting a random word
-@app.route('/get_winWord')
-def get_winWord():
-    winWord = random.choice(get_word_list())
-    return jsonify({'word': winWord})
+@app.route('/check_guess', methods=['POST'])
+def check_guess():
+    data = request.get_json()
+    guess = data.get('guess', '').upper()
+    correct_word = session.get('correct_word', '')
+    
+    display = ''
+    result = ''
+    
+    if len(guess) == len(correct_word):
+        display = ' '.join(['<span class="correct">{}</span>'.format(c) if c == g else g for c, g in zip(correct_word, guess)])
+        if guess == correct_word:
+            result = 'Congratulations! You guessed the word!'
+        else:
+            result = 'Incorrect guess. Try again.'
+    else:
+        result = 'Invalid guess. Please enter a word with the correct number of letters.'
+    
+    return jsonify({'display': display, 'result': result})
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.run(debug=True)
-
-
-    
