41275ed80a69b1f3fd368c98ce7a3cf48582e4e9
christophermorel <144291124+christophermorel@users.noreply.github.com>
2023-12-06 00:29:15 -0500

    e

--- static/wordle.js
+++ static/wordle.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let partialLettersShown = new Set();
 
     // Load word list from a file
-    fetch('/static/wordlist.txt')
+    fetch('/static/wordle.txt')  // Assuming the correct file name is wordle.txt
         .then(response => response.text())
         .then(data => {
             wordList = data.split('\n').map(word => word.trim()).filter(word => word.length > 0);
@@ -35,55 +35,58 @@ document.addEventListener("DOMContentLoaded", function () {
 
             console.log("Player Guess:", guess);
 
-            if (guess.length > 0) {
-                let correctLetters = 0;
+            if (wordList.includes(guess)) {
+                if (guess.length === randomWord.length) {
+                    let correctLetters = 0;
 
-                for (let i = 0; i < randomWord.length; i++) {
-                    if (i < guess.length && guess[i] === randomWord[i]) {
-                        correctLetters++;
-                        if (!correctLettersShown.has(guess[i])) {
-                            displayWord[i] = `<span class="correct">${randomWord[i]}</span>`;
-                            correctLettersShown.add(guess[i]);
-                        }
-                    } else if (randomWord.includes(guess[i]) && randomWord.indexOf(guess[i]) !== i) {
-                        // Check if the guessed letter is not in the correct position
-                        if (!partialLettersShown.has(guess[i]) && !correctLettersShown.has(guess[i])) {
-                            displayWord[i] = `<span class="partial">${guess[i]}</span>`;
-                            partialLettersShown.add(guess[i]);
+                    for (let i = 0; i < randomWord.length; i++) {
+                        if (i < guess.length && guess[i] === randomWord[i]) {
+                            correctLetters++;
+                            if (!correctLettersShown.has(guess[i])) {
+                                displayWord[i] = `<span class="correct">${randomWord[i]}</span>`;
+                                correctLettersShown.add(guess[i]);
+                            }
+                        } else if (randomWord.includes(guess[i]) && randomWord.indexOf(guess[i]) !== i) {
+                            if (!partialLettersShown.has(guess[i]) && !correctLettersShown.has(guess[i])) {
+                                displayWord[i] = `<span class="partial">${guess[i]}</span>`;
+                                partialLettersShown.add(guess[i]);
+                            }
                         }
                     }
-                }
 
-                console.log("Display Word:", displayWord.join(" "));
+                    console.log("Display Word:", displayWord.join(" "));
 
-                wordDisplay.innerHTML = displayWord.join(" ");
+                    wordDisplay.innerHTML = displayWord.join(" ");
 
-                if (correctLetters === randomWord.length) {
-                    resultDisplay.innerHTML = "Congratulations! You guessed the word!";
-                    guessInput.disabled = true;
-                } else {
-                    resultDisplay.innerHTML = `Incorrect guess. ${remainingAttempts} attempts remaining.`;
-                    remainingAttempts--;
-
-                    if (remainingAttempts === 0) {
-                        resultDisplay.innerHTML = `Sorry, you've run out of attempts. The correct word was ${randomWord}.`;
+                    if (correctLetters === randomWord.length) {
+                        resultDisplay.innerHTML = "Congratulations! You guessed the word!";
+                        guessHistory.push(`<div>${randomWord}</div>`); // Add correct guess to history
+                        guessHistoryDisplay.innerHTML = guessHistory.join("");
                         guessInput.disabled = true;
+                    } else {
+                        resultDisplay.innerHTML = `Incorrect guess. ${remainingAttempts} attempts remaining.`;
+                        remainingAttempts--;
+
+                        if (remainingAttempts === 0) {
+                            resultDisplay.innerHTML = `Sorry, you've run out of attempts. The correct word was ${randomWord}.`;
+                            guessInput.disabled = true;
+                        }
+
+                        const guessHistoryEntry = displayWord.map(char => char.includes('span') ? char : '_').join(" ");
+                        guessHistory.push(`<div>${guessHistoryEntry}</div>`);
+                        guessHistoryDisplay.innerHTML = guessHistory.join("");
+
+                        correctLettersShown.clear();
+                        partialLettersShown.clear();
                     }
-
-                    // Add only placeholders, correct, and partial guesses to the guess history
-                    const guessHistoryEntry = displayWord.map(char => char.includes('span') ? char : '_').join(" ");
-                    guessHistory.push(`<div>${guessHistoryEntry}</div>`);
-                    guessHistoryDisplay.innerHTML = guessHistory.join("");
-
-                    // Reset the sets of correct and partial letters shown for the next guess
-                    correctLettersShown.clear();
-                    partialLettersShown.clear();
+                } else {
+                    resultDisplay.innerHTML = "Please enter a word with the correct number of letters.";
                 }
             } else {
-                resultDisplay.innerHTML = "Please enter a word.";
+                resultDisplay.innerHTML = "Invalid guess. Please enter a valid word from the word list.";
             }
 
             guessInput.value = "";
         };
     }
-})
+});
