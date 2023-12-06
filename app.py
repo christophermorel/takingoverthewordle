600df7f4d4afacb8ececef4ce6d20809e1fbbfc0
christophermorel <144291124+christophermorel@users.noreply.github.com>
2023-12-06 01:21:31 -0500

    eee

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-# import random
-
-from flask import Flask, render_template, request
+import random
+from flask import Flask, render_template, request, jsonify
 from wordle_wordlist import get_word_list
-# from cpu.py import cpu_wordle
+from cpu import cpu_wordle
+
 
 app = Flask(__name__)
 
 # Home route - Renders the title screen or handles form submission
 @app.route('/', methods=['GET', 'POST'])
-
 def home():
     if request.method == 'POST':
         player_name = request.form.get('player_name', 'Guest')
         return render_template('wordle.html', player_name=player_name)
     return render_template('index.html')
+
 
 # Wordle route - Renders the play screen
 @app.route('/wordle', methods=['POST'])
@@ -21,15 +21,14 @@ def wordle():
     player_name = request.form.get('player_name', 'Guest')
     return render_template('wordle.html', player_name=player_name)
 
+# Route for getting a random word
+@app.route('/get_winWord')
+def get_winWord():
+    winWord = random.choice(get_word_list())
+    return jsonify({'word': winWord})
+
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-@app.route('/wordle')
-def wordle():
-    guess = request.form.get("guess")
-    if guess == winWord:
-        winner="you won"
-    return render_template('wordle.html', winner=winner)
 
     
